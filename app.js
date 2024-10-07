81da4a4ed202f89b3999bc11027f08466d7f3c49
Ric <74797847+rickatnite@users.noreply.github.com>
2024-10-07 18:58:46 -0400

    Changed all functions to fit new requirements with button selections.

--- app.js
+++ app.js
@@ -1,6 +1,50 @@
-let computerScore = 0;
-let humanScore = 0;
+// create buttons
+const rockButton = document.createElement("button");
+rockButton.textContent = "Rock";
 
+const paperButton = document.createElement("button");
+paperButton.textContent = "Paper";
+
+const scissorsButton = document.createElement("button");
+scissorsButton.textContent = "Scissors";
+
+const resetButton = document.createElement("button");
+resetButton.textContent = "Reset";
+
+// create div to display results 
+const resultsDiv = document.createElement("div");
+
+// append buttons to the body
+const body = document.querySelector("body");
+body.appendChild(rockButton);
+body.appendChild(paperButton);
+body.appendChild(scissorsButton);
+
+// add event listeners for the buttons
+rockButton.addEventListener("click", () => {
+    playRound("rock", getCompChoice());
+});
+paperButton.addEventListener("click", () => {
+    playRound("paper", getCompChoice());
+});
+scissorsButton.addEventListener("click", () => {
+    playRound("scissors", getCompChoice());
+});
+resetButton.addEventListener("click", () => {
+    resetGame();
+});
+
+// declare score variables
+let playerScore = 0;
+let computerScore = 0;
+
+// create and append scoring elements
+const displayResult = document.createElement("div");
+const displayScore = document.createElement("div");
+displayScore.textContent = ("Player: " + playerScore + " || " + "Computer: " + computerScore);
+body.appendChild(displayScore);
+
+// declare function to get computer choice
 function getCompChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
     let cChoice = "";
@@ -15,60 +59,73 @@ function getCompChoice() {
     return cChoice;
 }
 
-function getHumanChoice() {
-    let hChoice = prompt("Type rock, paper, or scissors to play!").toLowerCase();
-    console.log("Player: " + hChoice);
-    return hChoice;
-}
-
-function playGame() {
-    let i;
-    for (i = 0; i < 5; i++) {
-        let humanChoice = getHumanChoice();
-        let compChoice = getCompChoice();
-        playRound(humanChoice, compChoice);
-        console.log("Your Score: " + humanScore + "  ||  " + "Computer Score: " + computerScore);
+// declare function to play one round, increment scores, display and append results
+function playRound(humanChoice, compChoice) {
+    if (humanChoice === "rock" && compChoice === "paper") {
+        body.appendChild(displayResult);
+        displayResult.textContent = "You Lose! Paper Beats Rock";
+        computerScore++;
+        displayScore.textContent = "Player: " + playerScore + " || Computer: " + computerScore;
+    } else if (humanChoice === "rock" && compChoice === "scissors") {
+        body.appendChild(displayResult);
+        displayResult.textContent = "You Win! Rock Beats Scissors";
+        playerScore++;
+        displayScore.textContent = "Player: " + playerScore + " || Computer: " + computerScore;
+    } else if (humanChoice === "scissors" && compChoice === "paper") {
+        body.appendChild(displayResult);
+        displayResult.textContent = "You Win! Scissors Beats Paper";
+        playerScore++;
+        displayScore.textContent = "Player: " + playerScore + " || Computer: " + computerScore;
+    } else if (humanChoice === "scissors" && compChoice === "rock") {
+        body.appendChild(displayResult);
+        displayResult.textContent = "You Lose! Rock Beats Scissors";
+        computerScore++;
+        displayScore.textContent = "Player: " + playerScore + " || Computer: " + computerScore;
+    } else if (humanChoice === "paper" && compChoice === "rock") {
+        body.appendChild(displayResult);
+        displayResult.textContent = "You Win! Paper Beats Rock";
+        playerScore++;
+        displayScore.textContent = "Player: " + playerScore + " || Computer: " + computerScore;
+    } else if (humanChoice === "paper" && compChoice === "scissors") {
+        body.appendChild(displayResult);
+        displayResult.textContent = "You Lose! Scissors Beats Paper";
+        computerScore++;
+        displayScore.textContent = "Player: " + playerScore + " || Computer: " + computerScore;
+    } else if (humanChoice === compChoice) {
+        body.appendChild(displayResult);
+        displayResult.textContent = "It's A Tie! Go Again";
     }
-    function playRound(humanChoice, compChoice) {
-        if (humanChoice === "rock" && compChoice === "paper") {
-            computerScore++;
-            console.log("You Lose! Paper Beats Rock");
-            return;
-        } else if (humanChoice === "rock" && compChoice === "scissors") {
-            humanScore++;
-            console.log("You Win! Rock Beats Scissors");
-            return;
-        } else if (humanChoice === "scissors" && compChoice === "paper") {
-            humanScore++;
-            console.log("You Win! Scissors Beats Paper");
-            return;
-        } else if (humanChoice === "scissors" && compChoice === "rock") {
-            computerScore++;
-            console.log("You Lose! Rock Beats Scissors");
-            return;
-        } else if (humanChoice === "paper" && compChoice === "rock") {
-            humanScore++;
-            console.log("You Win! Paper Beats Rock");
-            return;
-        } else if (humanChoice === "paper" && compChoice === "scissors") {
-            computerScore++;
-            console.log("You Lose! Scissors Beats Paper");
-            return;
-        } else if (humanChoice === compChoice) {
-            console.log("It's A Tie! Go Again");
-            return;
+
+    // check to see if either player has reached five wins
+    if (playerScore === 5 || computerScore === 5) {
+        // disable buttons
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        scissorsButton.disabled = true;
+
+        // append reset button and results
+        body.appendChild(resetButton);
+        body.appendChild(resultsDiv);
+
+        // determine winner in results div
+        if (playerScore === 5) {
+            resultsDiv.textContent = "You won! Nice!";
+        } else {
+            resultsDiv.textContent = "You lost! Dang...";
         }
-        console.log("Player: " + humanChoice);
-        console.log("Computer: " + compChoice);
     }
-        if (humanScore === computerScore) {
-            console.log("It's a tie!");
-        } else if (humanScore > computerScore) {
-            console.log("You win!");
-        } else (console.log("You lose!"));
-        alert("Game over!");
-        alert("Your Score: " + humanScore + "  ||  " + "Computer Score: " + computerScore);
 }
 
-playGame();
+// declare function to reset the game and clear out variables
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    displayScore.textContent = "Player: " + playerScore + " || Computer: " + computerScore;
+    displayResult.textContent = "";
+    resultsDiv.textContent = "";
+    rockButton.disabled = false;
+    paperButton.disabled = false;
+    scissorsButton.disabled = false;
+    resetButton.remove();
+}
 
